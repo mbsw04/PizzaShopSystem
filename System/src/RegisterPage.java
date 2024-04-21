30d9971fa7081876100de441ed792f08ca641cce
Maan Bhagat <bhagatmaan04@gmail.com>
2024-04-21 00:19:10 -0400

    Revert "What i have done so far"
    This reverts commit f1ef844264ada23805b9a6c57ed365e449d813d5.

---
+++ System/src/RegisterPage.java
@@ -0,0 +1,186 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.FocusAdapter;
+import java.awt.event.FocusEvent;
+
+public class RegisterPage extends JDialog{
+    private JPanel RegPage;
+    private JTextField Phone;
+    private JTextField State;
+    private JTextField Zip;
+    private JTextField City;
+    private JTextField Apt;
+    private JButton BackBTN;
+    private JButton RegBTN;
+    private JLabel Logo;
+    private JLabel RegisteLabel;
+    private JTextField firstName;
+    private JTextField lastName;
+    private JTextField Email;
+    private JTextField Address;
+
+    ImageIcon logo1 = new ImageIcon("logo.png");
+
+    public RegisterPage(JFrame frame){
+        super(frame);
+        setTitle("Login");
+        setContentPane(RegPage);
+        setMinimumSize(new Dimension(1520,880));
+        setModal(true);
+        setIconImage(logo1.getImage());// Doesn't work gotta get a 16px icon !!
+        setLocationRelativeTo(frame);
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+
+        Phone.addFocusListener(new FocusAdapter() {
+            public void focusGained(FocusEvent e) {
+                if (Phone.getText().equals("Phone: (ex 1234567890)")) {
+                    Phone.setText("");
+                }
+            }
+            public void focusLost(FocusEvent e) {
+                if (Phone.getText().isEmpty()) {
+                    Phone.setText("Phone: (ex 1234567890)");
+                }
+            }
+        });
+
+        State.addFocusListener(new FocusAdapter() {
+            public void focusGained(FocusEvent e) {
+                if (State.getText().equals("State: (ex GA)")) {
+                    State.setText("");
+                }
+            }
+            public void focusLost(FocusEvent e) {
+                if (State.getText().isEmpty()) {
+                    State.setText("State: (ex GA)");
+                }
+            }
+        });
+
+        Zip.addFocusListener(new FocusAdapter() {
+            public void focusGained(FocusEvent e) {
+                if (Zip.getText().equals("Zip: (ex 111111)")) {
+                    Zip.setText("");
+                }
+            }
+            public void focusLost(FocusEvent e) {
+                if (Zip.getText().isEmpty()) {
+                    Zip.setText("Zip: (ex 111111)");
+                }
+            }
+        });
+
+        City.addFocusListener(new FocusAdapter() {
+            public void focusGained(FocusEvent e) {
+                if (City.getText().equals("City: (Ex Marietta)")) {
+                    City.setText("");
+                }
+            }
+            public void focusLost(FocusEvent e) {
+                if (City.getText().isEmpty()) {
+                    City.setText("City: (Ex Marietta)");
+                }
+            }
+        });
+
+        Apt.addFocusListener(new FocusAdapter() {
+            public void focusGained(FocusEvent e) {
+                if (Apt.getText().equals("Apt: (ex 111)")) {
+                    Apt.setText("");
+                }
+            }
+            public void focusLost(FocusEvent e) {
+                if (Apt.getText().isEmpty()) {
+                    Apt.setText("Apt: (ex 111)");
+                }
+            }
+        });
+
+        firstName.addFocusListener(new FocusAdapter() {
+            public void focusGained(FocusEvent e) {
+                if (firstName.getText().equals("First Name")) {
+                    firstName.setText("");
+                }
+            }
+            public void focusLost(FocusEvent e) {
+                if (firstName.getText().isEmpty()) {
+                    firstName.setText("First Name");
+                }
+            }
+        });
+
+        lastName.addFocusListener(new FocusAdapter() {
+            public void focusGained(FocusEvent e) {
+                if (lastName.getText().equals("Last Name")) {
+                    lastName.setText("");
+                }
+            }
+            public void focusLost(FocusEvent e) {
+                if (lastName.getText().isEmpty()) {
+                    lastName.setText("Last Name");
+                }
+            }
+        });
+
+        Email.addFocusListener(new FocusAdapter() {
+            public void focusGained(FocusEvent e) {
+                if (Email.getText().equals("Email: (ex 123@gmail.com)")) {
+                    Email.setText("");
+                }
+            }
+            public void focusLost(FocusEvent e) {
+                if (Email.getText().isEmpty()) {
+                    Email.setText("Email: (ex 123@gmail.com)");
+                }
+            }
+        });
+
+        Address.addFocusListener(new FocusAdapter() {
+            public void focusGained(FocusEvent e) {
+                if (Address.getText().equals("Address: (ex 1234 Pizza Street)")) {
+                    Address.setText("");
+                }
+            }
+            public void focusLost(FocusEvent e) {
+                if (Address.getText().isEmpty()) {
+                    Address.setText("Address: (ex 1234 Pizza Street)");
+                }
+            }
+        });
+
+/*
+        UserSwitch1Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+*/
+        BackBTN.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                setVisible(false);
+                dispose();
+                new EnterPage(null);
+            }
+        });
+
+        RegBTN.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                setVisible(false);
+                dispose();
+                new EnterPage(null);
+            }
+        });
+        setVisible(true);
+
+    }
+
+    //public static void main(String[] args) {
+    //RegisterPage test = new RegisterPage(null);
+    //}
+
+}
---
+++ System/src/ThankYou.java
@@ -0,0 +1,54 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+public class ThankYou{
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Thank You");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(null);
+        frame.setSize(750, 750);
+        frame.getContentPane().setBackground(Color.WHITE);
+
+        JPanel panel = new JPanel();
+        panel.setBackground(Color.cyan);
+        panel.setBounds(100,100,550,200);
+
+        JButton button = new JButton();
+        button.setBackground(Color.darkGray);
+        button.setBounds(175,350,400,100);
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                System.exit(0);
+            }
+        });
+
+        JLabel text = new JLabel("Thank You for placing your order!");
+        text.setHorizontalTextPosition(JLabel.CENTER);
+        text.setVerticalTextPosition(JLabel.TOP);
+        text.setForeground(new Color(0x000000));
+        text.setFont(new Font("MV Boli", Font.PLAIN, 25));
+
+        JLabel txt = new JLabel("Your food should be ready in 20-35 minutes.");
+        txt.setHorizontalTextPosition(JLabel.CENTER);
+        txt.setVerticalTextPosition(JLabel.BOTTOM);
+        txt.setForeground(new Color(0x000000));
+        txt.setFont(new Font("MV Boli", Font.PLAIN, 25));
+
+        JLabel txt1 = new JLabel("Return to Home");
+        txt1.setHorizontalTextPosition(JLabel.CENTER);
+        txt1.setVerticalTextPosition(JLabel.CENTER);
+        txt1.setForeground(new Color(0x000000));
+        txt1.setFont(new Font("MV Boli", Font.PLAIN, 25));
+
+        frame.add(panel);
+        frame.add(button);
+        panel.add(text);
+        panel.add(txt);
+        button.add(txt1);
+        frame.setVisible(true);
+    }
+}
+
